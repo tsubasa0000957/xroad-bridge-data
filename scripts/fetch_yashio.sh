b404e89020370db7fab0f57240a107d154c5517d
tsubasa0000957 <tsubasa.ota@gmail.com>
2026-08-06 21:29:01 +0900

    fix: data/rawにfechデータを保存するように修正 (#2)

--- scripts/fetch_yashio.sh
+++ scripts/fetch_yashio.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 set -euo pipefail
 
-curl -sS \
-  "https://road-structures-db.mlit.go.jp/xROAD/api/v1/bridges?city=112348&limit=170&offset=0" -o yashio.json
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+PROJECT_ROOT=$(cd "$SCRIPT_DIR/.." && pwd)
+
+RAW_DIR="$PROJECT_ROOT/data/raw"
+
+mkdir -p "$RAW_DIR"
+
+curl -sSo \
+  "$RAW_DIR/yashio.json" "https://road-structures-db.mlit.go.jp/xROAD/api/v1/bridges?city=112348&limit=170&offset=0"
+
+jq . "$RAW_DIR/yashio.json" > "$RAW_DIR/tmp" \
+  && mv "$RAW_DIR/tmp" "$RAW_DIR/yashio.json"
